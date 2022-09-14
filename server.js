6589d8e3bb78153e0a7103ce6376e5af1bfb522f
bhavya <bhavyajain817@gmail.com>
2022-09-14 20:52:40 +0530

    Letsgo

--- server.js
+++ server.js
@@ -11,7 +11,8 @@ const connectDB = require('./server/database/connection');
 const app = express(); 
 
 dotenv.config({path:'config.env'})
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 8080
+
 
 
 // log requests
